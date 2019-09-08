64415f1b5127810beaeaa5ee9cc4a5ee9781f9d0
dobrien <dobrien@todopatterson.com>
2019-09-08 21:16:15 +0200

    hello function

---
+++ functions/postman.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+exports.handler = function(event, context, callback) {
+  const parseBody = JSON.parse(event.body)
+  console.log(parseBody)
+  axios({
+    method: 'post',
+    url: 'https://enj521z9xemsn.x.pipedream.net/',
+    data: { name: parseBody.name },
+  })
+    .then((response) => {
+      console.log(response)
+      callback(null, {
+        statusCode: 200,
+        body: 'Netlify funcitons work just so easy oh yeah',
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      callback(new Error('someting went wrong'))
+    })
+}
