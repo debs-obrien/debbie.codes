6c2154aba9dbbf4840ce0adfaf25b279ba7ce171
dobrien <dobrien@todopatterson.com>
2020-01-26 19:58:27 +0100

    filter added to conferences

--- pages/conferences/index.vue
+++ pages/conferences/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="container mt-10">
     <h1 class="main-heading">conferences</h1>
-    <div v-for="(conf, index) in conferences" :key="index" class="flex">
+    <div class="buttons mb-4">
+      <button class="grax_button" @click="FilterConferenceByType('')">
+        All
+      </button>
+      <button class="grax_button" @click="FilterConferenceByType('Speaker')">
+        Speaker
+      </button>
+      <button class="grax_button" @click="FilterConferenceByType('MC')">
+        MC
+      </button>
+      <button class="grax_button" @click="FilterConferenceByType('Attendee')">
+        Attendee
+      </button>
+    </div>
+    <div v-for="(conf, index) in conferenceList" :key="index" class="flex">
       <ConferenceLinks
         :img="conf.img"
         :alt="conf.alt"
@@ -31,7 +45,18 @@ export default {
   data() {
     return {
       conferences: conferences.conferences,
+      type: '',
     }
+  },
+  computed: {
+    conferenceList() {
+      return this.conferences.filter((el) => el.type.match(this.type))
+    },
+  },
+  methods: {
+    FilterConferenceByType(type) {
+      this.type = type
+    },
   },
 }
 </script>
