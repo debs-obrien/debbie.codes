e839b2e240d8c614412eeb1fb34444fca54f93ff
debs_obrien <debs_obrien@yahoo.co.uk>
2020-05-16 19:18:36 +0200

    fix animations

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -69,19 +69,17 @@ export default {
     }
   },
   mounted() {
-    gsap.fromTo(
-      '.card',
-      { opacity: 0 },
-      {
-        opacity: 1,
-        duration: 0.5,
-        ease: 'power1.out',
-        stagger: {
-          each: 1.5,
-          from: 'bottom'
-        }
+    gsap.from('.card', {
+      duration: 0.5,
+      opacity: 0,
+      scale: 0,
+      y: 200,
+      ease: 'power1',
+      stagger: {
+        each: 0.1,
+        from: 'center'
       }
-    )
+    })
   },
   methods: {
     lazyLoadArticles(isVisible) {
