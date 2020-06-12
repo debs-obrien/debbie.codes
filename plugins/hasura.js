6aaab548676291207433f47102a8f7efb88a8b50
debs-obrien <debs_obrien@yahoo.co.uk>
2020-06-12 17:34:17 +0200

    add hasura plugin

--- plugins/hasura.js
+++ plugins/hasura.js
@@ -3,7 +3,5 @@ export default function ({ $http, env }, inject) {
     prefixUrl: env.API_HASURA_URL
   })
 
-  $hasura.setHeader('x-hasura-admin-secret', env.Hasura_Admin_Secret_Key)
-
   inject('hasura', $hasura.$post.bind($hasura, ''))
 }
