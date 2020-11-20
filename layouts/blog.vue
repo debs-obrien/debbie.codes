cce3efe80102d777fea47ef636dc65c258d1f02b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-20 20:38:57 +0100

    feat: add twitter to blog

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -9,3 +9,34 @@
     <TheFooter />
   </div>
 </template>
+<script>
+  export default {
+    head: {
+      meta: [
+        { name: 'twitter:site', content: '@debs_obrien' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        {
+          hid: 'twitter:url',
+          name: 'twitter:url',
+          content: 'https://debbie.codes.com'
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: "Welcome to Debbie's blog"
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:
+            'Blog with lots of cool articles and tips on Nuxt and tech in general'
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: '/icon.png'
+        }
+      ]
+    }
+  }
+</script>
