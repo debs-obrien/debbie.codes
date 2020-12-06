5454e9c200393bb34092b8e2d0cad776924d01ae
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-12-06 21:47:31 +0100

    fix: light mode resources

--- components/ResourcesCard.vue
+++ components/ResourcesCard.vue
@@ -2,7 +2,7 @@
   <div class="pattern-dots-md gray-light">
     <NuxtLink :to="resource.link">
       <div
-        class="rounded bg-gray-800 p-4 transform translate-x-6 -translate-y-6  "
+        class="rounded bg-white dark:bg-gray-800  p-4 transform translate-x-6 -translate-y-6  "
       >
         <!-- <div
           class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-green-100 text-green-500 mb-5 flex-shrink-0 p-2"
