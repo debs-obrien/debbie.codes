40ebd4b2d0670d4aa2608d003af64cf954358c3e
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-02-02 17:55:10 +0100

    fixes: prev next

--- components/ArticlePrevNext.vue
+++ components/ArticlePrevNext.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="flex justify-between">
-    <PostsCard v-if="prev" :item="prev" class="prev mr-2" />
+  <div class="flex flex-col md:flex-row justify-between">
+    <PrevNextPostsCard
+      v-if="prev"
+      :item="prev"
+      class="prev xs:mb-8 md:mb-0 md:mr-2"
+    />
     <span v-else>&nbsp;</span>
-    <PostsCard v-if="next" :item="next" class="next ml-2" />
+    <PrevNextPostsCard v-if="next" :item="next" class="next md:ml-2" />
     <span v-else>&nbsp;</span>
   </div>
 </template>
---
+++ components/PrevNextPostsCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+    <div class="flex-1 bg-white px-4 pb-2 flex justify-between">
+      <NuxtLink :to="`/blog/${item.slug}`" class="block">
+        <h4
+          class="mt-2 text-xl leading-7 font-semibold text-gray-900 hover:text-gray-600"
+        >
+          {{ item.title }}
+        </h4>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
