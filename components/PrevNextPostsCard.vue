8982c2ac4168b8f7fee445975896064f7f57504e
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-02-12 21:11:11 +0100

    fix: padding prev next

--- components/PrevNextPostsCard.vue
+++ components/PrevNextPostsCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-    <div class="flex-1 bg-white px-4 pb-2 flex justify-between">
+  <div class="flex flex-col ">
+    <div
+      class="flex-1 bg-white px-4 mb-8 pb-2 flex justify-between rounded-lg shadow-lg overflow-hidden"
+    >
       <NuxtLink :to="`/blog/${item.slug}`" class="block">
         <h4
           class="mt-2 text-xl leading-7 font-semibold text-gray-900 hover:text-gray-600"
