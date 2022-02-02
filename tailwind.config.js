3f3d9709ca9ec04472dc1ac8c8c1e7e8b719536b
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2022-02-02 20:10:38 +0100

    remove old comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,7 @@
 module.exports = {
   content: ['./**.{vue,js}'],
   darkMode: 'class',
-  // darkMode: 'class',
-  // purge: {
-  //   // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-  //   enabled: process.env.NODE_ENV === 'production',
-  //   content: [
-  //     'components/**/*.vue',
-  //     'layouts/**/*.vue',
-  //     'pages/**/*.vue',
-  //     'plugins/**/*.js',
-  //     'nuxt.config.js'
-  //   ],
-  //   options: {
-  //     whitelist: []
-  //   }
-  // },
+
   theme: {
     darkSelector: '.dark',
     extend: {
