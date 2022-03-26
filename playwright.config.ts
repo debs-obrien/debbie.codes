a22da793a97d05b4fb839aae9504b2c903b1cb0a
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2022-03-26 15:37:27 +0100

    fix test base urls

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on'
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,46 +1,84 @@
-import { test, expect, chromium } from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  // Go to https://debbie.codes/
-  await page.goto('https://debbie.codes/')
+  // Go to http://localhost:8000/
+  await page.goto('http://localhost:8000/')
 
   // Click text=Blog >> nth=0
-  await page.locator('text=Blog').first().click()
-  await expect(page).toHaveURL('https://debbie.codes/blog')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog' }*/),
+    page.locator('text=Blog').first().click()
+  ])
 
   // Click text=Next page
-  await page.locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/2')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/all/2' }*/),
+    page.locator('text=Next page').click()
+  ])
 
   // Click text=Nuxt >> nth=1
-  await page.locator('text=Nuxt').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/Nuxt/1')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/Nuxt/1' }*/),
+    page.locator('text=Nuxt').nth(1).click()
+  ])
+
+  // Click text=Pagination in Nuxt Content
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/Nuxt/1' }*/),
+    page.locator('text=Pagination in Nuxt Content').click()
+  ])
 
   // Click text=Next page
-  await page.locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/Nuxt/2')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/Nuxt/2' }*/),
+    page.locator('text=Next page').click()
+  ])
 
-  // Click text=Prev page
-  await page.locator('text=Prev page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/Nuxt/1')
+  // Click text=Adding analytics to your Nuxt site
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/nuxt-analytics' }*/),
+    page.locator('text=Adding analytics to your Nuxt site').click()
+  ])
 
-  // Click a:has-text("React")
-  await page.locator('a:has-text("React")').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/React/1')
+  // Click [aria-label="Breadcrumb"] >> text=blog
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog' }*/),
+    page.locator('[aria-label="Breadcrumb"] >> text=blog').click()
+  ])
+
+  // Click text=React >> nth=1
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/React/1' }*/),
+    page.locator('text=React').nth(1).click()
+  ])
+
+  // Click text=Theming in React
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/theming-in-react' }*/),
+    page.locator('text=Theming in React').click()
+  ])
+
+  // Click [aria-label="Breadcrumb"] >> text=blog
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog' }*/),
+    page.locator('[aria-label="Breadcrumb"] >> text=blog').click()
+  ])
 
   // Click text=Next page
-  await page.locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/React/2')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/all/2' }*/),
+    page.locator('text=Next page').click()
+  ])
 
   // Click text=All >> nth=1
-  await page.locator('text=All').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/1')
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/all/1' }*/),
+    page.locator('text=All').nth(1).click()
+  ])
 
-  // Click text=Next page
-  await page.locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/2')
-
-  // Click text=Prev page
-  await page.locator('text=Prev page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/1')
+  // Click text=Pagination in Nuxt Content
+  await Promise.all([
+    page.waitForNavigation(/*{ url: 'http://localhost:8000/blog/pagination-in-nuxt-content' }*/),
+    page.locator('text=Pagination in Nuxt Content').click()
+  ])
 })
