e4e45eb4be85e021a101f605aa84df26a2685b26
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2022-04-21 08:44:03 +0200

    fix pagination tests

--- tests/pagination.spec.ts
+++ tests/pagination.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test('test', async ({ page }) => {
+test('pagination of blog', async ({ page }) => {
   // Go to https://debbie.codes/
   await page.goto('https://debbie.codes/')
 
@@ -8,68 +8,40 @@ test('test', async ({ page }) => {
   await page.locator('text=Blog').first().click()
   await expect(page).toHaveURL('https://debbie.codes/blog')
 
-  // Click text=nuxt >> nth=1
-  await page.locator('text=nuxt').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/nuxt/1')
+  // Click text=Next page
+  await page.locator('text=Next page').click()
+  await expect(page).toHaveURL('https://debbie.codes/blog/all/2')
 
-  // Click text=Pagination in Nuxt Content
-  await page.locator('text=Pagination in Nuxt Content').click()
-  await expect(page).toHaveURL(
-    'https://debbie.codes/blog/pagination-in-nuxt-content'
-  )
+  // Click text=Prev page
+  await page.locator('text=Prev page').click()
+  await expect(page).toHaveURL('https://debbie.codes/blog/all/1')
 
-  // Click text=Home blog Pagination in Nuxt Content Pagination in Nuxt Content Nuxt content is
-  await page
-    .locator(
-      'text=Home blog Pagination in Nuxt Content Pagination in Nuxt Content Nuxt content is '
-    )
-    .click()
+  // Click text=Being an Imposter
+  await page.locator('text=Being an Imposter').click()
+  await expect(page).toHaveURL('https://debbie.codes/blog/being-an-imposter')
+
+  // Click h1:has-text("Being an Imposter")
+  await page.locator('h1:has-text("Being an Imposter")').click()
 
   // Click [aria-label="Breadcrumb"] >> text=blog
   await page.locator('[aria-label="Breadcrumb"] >> text=blog').click()
   await expect(page).toHaveURL('https://debbie.codes/blog')
 
-  // Click text=Next page
-  await page.locator('#prev-next').locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/2')
-
-  // Click text=nuxt >> nth=1
-  await page.locator('text=nuxt').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/nuxt/1')
-  await page.locator('Pagination in Nuxt Content')
-
-  // Click a:has-text("react")
-  await page.locator('a:has-text("react")').click()
+  // Click text=react >> nth=1
+  await page.locator('text=react').nth(1).click()
   await expect(page).toHaveURL('https://debbie.codes/blog/react/1')
 
-  // Click text=Next page
-  await page.locator('#prev-next').locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/react/2')
-  await page.locator('text=Learning React')
-
-  // Click text=All >> nth=1
-  await page.locator('text=All').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/1')
-
-  // Click text=Next page
-  await page.locator('#prev-next').locator('text=Next page').click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/all/2')
-
-  // Click text=dev stuff
+  // Click text=Design Tokens and Theming
   await Promise.all([
-    page.waitForNavigation(/*{ url: 'https://debbie.codes/blog/dev%20stuff/1' }*/),
-    page.locator('text=dev stuff').click()
+    page.waitForNavigation(/*{ url: 'https://debbie.codes/blog/react/1' }*/),
+    page.locator('text=Design Tokens and Theming').click()
   ])
 
-  // Click text=typescript >> nth=0
-  await page.locator('text=typescript').first().click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/typescript/1')
+  // Click text=Next page
+  await page.locator('text=Next page').click()
+  await expect(page).toHaveURL('https://debbie.codes/blog/react/2')
 
-  // Click text=javascript >> nth=1
-  await page.locator('text=javascript').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/javascript/1')
-
-  // Click a:has-text("testing")
-  await page.locator('a:has-text("testing")').nth(1).click()
-  await expect(page).toHaveURL('https://debbie.codes/blog/testing/1')
+  // Click text=Prev page
+  await page.locator('text=Prev page').click()
+  await expect(page).toHaveURL('https://debbie.codes/blog/react/1')
 })
