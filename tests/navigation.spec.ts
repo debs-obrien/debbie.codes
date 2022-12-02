1a744258a8f3a334b882652d31ed3e6ef3b1ac0f
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2022-12-02 09:16:56 +0100

    tests: fix nav tests

--- tests/navigation.spec.ts
+++ tests/navigation.spec.ts
@@ -2,7 +2,12 @@ import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
-})
+});
+
+test('logo links to home page', async ({ page }) => {
+  await page.getByRole('link', { name: 'Debbie O\'Brien Debbie O\'Brien' }).click();
+  await expect(page).toHaveURL('/');
+});
 
 test('navigation in header works', async ({ page }) => {
   const navigation = page.getByRole('navigation')
@@ -21,9 +26,6 @@ test('navigation in header works', async ({ page }) => {
 
   await navigation.getByRole('link', { name: 'Blog' }).click();
   await expect(page).toHaveURL('blog');
-
-  await page.getByRole('link', { name: 'Debbie O\'Brien' }).click();
-  await expect(page).toHaveURL('/');
 });
 
 test('navigation in footer works', async ({ page }) => {
@@ -43,7 +45,4 @@ test('navigation in footer works', async ({ page }) => {
 
   await navigation.getByRole('link', { name: 'Blog' }).click();
   await expect(page).toHaveURL('blog');
-
-  await page.getByRole('link', { name: 'Debbie O\'Brien' }).click();
-  await expect(page).toHaveURL('/');
 });
