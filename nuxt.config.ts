1922877e060c1e568831ba350bcdf6d0d0d46bd0
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2025-02-24 21:50:14 +0100

    chore: update dependencies and clean up sitemap route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
   },
+
   modules: [
-    '@nuxt/image-edge',
+    '@nuxt/image',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
     '@nuxt/content',
@@ -15,10 +18,12 @@ export default defineNuxtConfig({
     preference: 'system', // default value of $colorMode.preference
     fallback: 'dark',
   },
+
   css: [
     '~/assets/css/main.css',
     '~/node_modules/lite-youtube-embed/src/lite-yt-embed.css',
   ],
+
   image: {
     cloudinary: {
       baseURL: 'https://res.cloudinary.com/debsobrien/image/upload/',
@@ -37,23 +42,25 @@ export default defineNuxtConfig({
       },
     },
   },
+
   content: {
-    highlight: {
-      theme: 'github-dark'
-    },
   },
-   nitro: {
+
+  nitro: {
     prerender: {
       routes: ['/sitemap.xml']
     }
   },
+
   build: {
     transpile: ['lite-youtube'],
   },
+
   vue: {
     compilerOptions: {
       isCustomElement: tag => ['lite-youtube'].includes(tag),
     },
   },
-})
 
+  compatibilityDate: '2025-02-24',
+})
