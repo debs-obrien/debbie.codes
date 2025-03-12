8bdedf38be6cbe4ec988d8b7b285f6d256946a26
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2025-03-12 21:04:06 +0100

    feat: add id prop to AppSubtitle component and improve default value handling in blog slug page

--- components/AppSubtitle.vue
+++ components/AppSubtitle.vue
@@ -1,7 +1,12 @@
 <template>
   <h2
+    :id="id"
     class="font-sans text-center mt-16 text-3xl font-semibold sm:text-4xl sm:leading-none md:text-5xl"
   >
     <slot>Title</slot>
   </h2>
 </template>
+
+<script setup lang="ts">
+defineProps<{ id: string }>()
+</script>
