e65dd2aefa06a278a0a4df02e1d8defb444105e1
Daniel Roe <daniel@roe.dev>
2025-03-13 21:56:54 +0100

    chore: wait for localhost

--- playwright.config.ts
+++ playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://127.0.0.1:3020',
+    url: 'http://localhost:3020',
     reuseExistingServer: !process.env.CI,
   },
 });
