9bccf22117ccf7fedc4c670c5da976d46eb5cedd
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2025-11-17 20:41:19 +0100

    style: update BlogSearch component for improved search input design and layout

--- components/BlogSearch.vue
+++ components/BlogSearch.vue
@@ -56,16 +56,16 @@ watch(() => props.articles, () => {
 </script>
 
 <template>
-  <div class="max-w-2xl mx-auto mt-6 mb-4">
+  <div class="max-w-xl mx-auto mt-6 mb-4">
     <div class="relative">
       <input
         v-model="searchQuery"
         type="search"
-        placeholder="Search articles..."
-        class="w-full px-4 py-2 pl-10 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500 bg-white dark:bg-gray-800"
+        placeholder="Search..."
+        class="w-full px-4 py-2 pl-9 text-sm border border-gray-300 dark:border-gray-600 rounded-full bg-gray-100 dark:bg-gray-800/50 focus:outline-none focus:ring-1 focus:ring-gray-400 dark:focus:ring-gray-500 placeholder-gray-400 dark:placeholder-gray-500 transition-all"
       />
       <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+        <svg class="h-4 w-4 text-gray-400 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
         </svg>
       </div>
