95e8432ed9bc3a86de5fdd6e05cfce08edcc4840
Debbie O'Brien <debbie@zephyr-cloud.io>
2026-06-26 17:37:44 +0200

    test: fix award link popup handling

--- tests/award-links.spec.ts
+++ tests/award-links.spec.ts
@@ -15,7 +15,7 @@ test('google gde link in home page works', async ({ page }) => {
 
   const [page1] = await Promise.all([
     page.waitForEvent('popup'),
-    await page.getByRole('link', { name: 'Google GDE' }).click()
+    page.getByRole('link', { name: 'Google GDE' }).click()
   ]);
   await expect(page1).toHaveURL('https://me.developers.google.com/u/115790798136433531532');
 });
@@ -27,7 +27,7 @@ test('microsoft mvp link in home page works', async ({ page }) => {
 
   const [page1] = await Promise.all([
     page.waitForEvent('popup'),
-    await page.getByRole('link', { name: 'Former Microsoft MVP' }).click()
+    page.getByRole('link', { name: 'Former Microsoft MVP' }).click()
   ]);
   await expect(page1).toHaveURL('https://mvp.microsoft.com/en-us');
 });
@@ -39,7 +39,7 @@ test('GitHub Star link in home page works', async ({ page }) => {
 
   const [page1] = await Promise.all([
     page.waitForEvent('popup'),
-    await page.getByRole('link', { name: 'GitHub Star Alumni' }).click()
+    page.getByRole('link', { name: 'GitHub Star Alumni' }).click()
   ]);
   await expect(page1).toHaveURL('https://stars.github.com/alumni/');
 });
@@ -51,7 +51,7 @@ test('Nuxt Ambassador link in home page works', async ({ page }) => {
 
   const [page1] = await Promise.all([
     page.waitForEvent('popup'),
-    await page.getByRole('link', { name: 'Nuxt Ambassador' }).click()
+    page.getByRole('link', { name: 'Nuxt Ambassador' }).click()
   ]);
   await expect(page1).toHaveURL('https://nuxtjs.org/teams/');
 });
